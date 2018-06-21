1fa0cbb064e4bc702e77bcd4383ee2c348a6f721
Adriana <adpc1609@gmail.com>
2018-06-21 17:00:09 -0400

    modified data,index,main

--- src/main.js
+++ src/main.js
@@ -1,31 +1,70 @@
+const url = 'https://api.laboratoria.la/';
 
-const url1 = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
-const url2 = '../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
-const url = 'https://laboratoria-la-staging.firebaseapp.com';
 
-/* Se debe obtener la data de API desde este archivo */
-const data = loadData.getCampuses(url + '/campuses').then(response =>{
-    console.log(response);
+const data = loadData.getCampuses(url).then(response => {
+   
     let campuses = document.getElementById("campus");
-    console.log(campuses);
-    for(let item in response ){
-        // let etiqueta = document.createElement('a');
-        
-        campuses.innerHTML += '<a class="dropdown-item" onclick="'+getCohortsByIid(response[item].id)+'" href="#">'+response[item].name+'</a>';
-        
+   
+    campuses.innerHTML = '';
+    for (let item in response) {
+        //let etiqueta = document.createElement('a');
+        campuses.innerHTML += '<a class="dropdown-item" onmouseover="+getCohortsByIid(\''+response[item]+'\');" href="#">'+response[item].name+'</a>';
     }
+
+    const dataCoh = loadData.getCohortsByIid(url + 'cohorts').then(response => {
+        console.log(response);
+        let cohorts = document.getElementById("cohort");
+        console.log(cohorts);
+         for (let item in response) {
+           cohorts.innerHTML += '<a class="dropdown-item "onclick="' + getAlumnas(response[item].id) + '" href="#">' + response[item].name + '</a';
+        }
+    
+    })
+
+    const dataUsers = loadData.getUserLab(url + 'cohorts/lim-2017-09-bc-core-am/users').then(response => {
+        console.log(response);
+        let Users = document.getElementById("alumnas");
+        console.log(Users);
+         for (let item in response) {
+           Users.innerHTML += '<a class="dropdown-item "onclick="' +getAlumnas(response[item].id) + '" href="#">' + response[item].name + '</a';
+        }
+    
+    })
+
+    /*const dataUsers = loadData.getProgress(url + 'progress').then(response => {
+        console.log(response);
+        let Users = document.getElementById("alumnas");
+        console.log(Users);
+         for (let item in response) {
+           Users.innerHTML += '<a class="dropdown-item "onclick="' +getAlumnas(response[item].id) + '" href="#">' + response[item].name + '</a';
+        }
+    
+    })*/
+})    
+
+
+
 function getCohortsByIid(id){
     console.log(id);
+    loadData.getCohortsByIid(url + 'cohorts' + 'campus='+id).then(response => {
+        console.log(response);
+        let cohort = document.getElementById("cohort");
+        console.log(cohort);
+        cohort.innerHTML='';
+        for (let item in response) {
+            cohort.innerHTML += '<a class="dropdown-item dropdown-toggle" onmouseover="+getAlumnas(\''+response[item].id+'\');" href="#">'+response[item].id+'</a>';
+        }
+    })
+
 }
-  
 
+function getAlumnas(id){
+    console.log(id);
+}
 
-});
-
-
-
-   
-
+function getUser(id){
+    console.log(id);
+}
 /*function loadAllData(url){
 
     // Concadena las url para enviar a la api 
